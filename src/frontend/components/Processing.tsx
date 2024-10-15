7ffff13f16130a788d06d6c8f2ab8aff203a1fba
GeorgeAlexsander <111101371+GeorgeAlexsander@users.noreply.github.com>
2024-10-15 02:14:23 -0300

    Alterado tela de processamento
    - Instalado React-spinners (para animações de loading)
    - Inseri uma animação de carregando com um pacman
    - Pode-se trocar por algo mais normal caso necessário, é só trocar a linha de escolha.
    
    Futuramente vai trocar o texto do "analisando sua foto"
    Vai dizer (de forma leiga) o que estamos fazendo
    "Estamos analisando a sua foto"
    "Falta pouco"
    "Nossa inteligência artificial está trabalhando"
    ou algo só pra indicar que está fazendo algo e que não travou.

--- src/frontend/components/Processing.tsx
+++ src/frontend/components/Processing.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import PacmanLoader from 'react-spinners/PacmanLoader'; // Importa o PacmanLoader
 
 const Processing: React.FC = () => {
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      const container = document.querySelector('.container');
-      container?.classList.add('fade-out');
-      setTimeout(() => navigate('/result'), 500); // Redireciona após fade-out
+      setLoading(false);
+      navigate('/result');
     }, 3000); // Simula 3 segundos de processamento
     return () => clearTimeout(timer);
   }, [navigate]);
@@ -16,8 +17,17 @@ const Processing: React.FC = () => {
   return (
     <div className="container fade-in processing-container">
       <h1>Processando...</h1>
-      <div className="animation">🔄</div>
-      <p>Estamos analisando a sua foto.</p>
+      <div className="spinner">
+        <div className="pacman-wrapper">
+          <PacmanLoader
+            color="#1a2a6c"        // Cor do PacmanLoader
+            loading={loading}       // Controla a exibição
+            size={30}               // Tamanho do loader
+            speedMultiplier={0.9}   // Aumenta a velocidade da animação
+          />
+        </div>
+      </div>
+      <p className="processing-text">Estamos analisando a sua foto.</p>
     </div>
   );
 };
