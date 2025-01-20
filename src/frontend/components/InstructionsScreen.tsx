fd4781116543ad675fa6e838bd71a9df89f205a7
GeorgeAlexsander <111101371+GeorgeAlexsander@users.noreply.github.com>
2025-01-20 02:49:18 -0300

    teste

--- src/frontend/components/InstructionsScreen.tsx
+++ src/frontend/components/InstructionsScreen.tsx
@@ -31,7 +31,7 @@ const InstructionsScreen: React.FC = () => {
   };
 
   return (
-    <div className="container result-container">
+    <div className="container fade-in instructions-screen">
       <h1>Instruções de Uso</h1>
       <Slider ref={sliderRef} {...settings}>
         <div className="carousel-slide">
